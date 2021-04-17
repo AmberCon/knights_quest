f1576cbd568182c6065ac7a9147a8ca531d9ad76
Ember Chan <calvinjchan1@email.arizona.edu>
2021-04-16 20:08:41 -0700

    Documentation for model class

--- model/StrategyGameModel.java
+++ model/StrategyGameModel.java
@@ -1,5 +1,27 @@
 package model;
 
+/**
+ * This class represents the model in the MCV architecture for
+ * this strategy game.
+ * 
+ * The coordinates of the board for the game are as such:
+ * 
+ * 0 1 2 3 4 5 6 . . . x 
+ * 1
+ * 2
+ * 3
+ * 4
+ * 5
+ * 6
+ * .
+ * .
+ * .
+ * y
+ * 
+ * 
+ * @author Ember Chan
+ *
+ */
 public class StrategyGameModel{
-	
+	private Tile[][] board;
 }
