7939a123fe5848f1c21f85d096ffdad6a5648865
Saul Weintraub <siwsiwsiw6@gmail.com>
2021-04-17 14:49:45 -0700

    preparing to merge with model

--- controller/StrategyGameController.java
+++ controller/StrategyGameController.java
@@ -1,12 +1,15 @@
 package controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import model.StrategyGameModel;
 
 public class StrategyGameController{
 	StrategyGameModel model;
 	
 	public StrategyGameController(StrategyGameModel model) {
-		
+		this.model = model;
 	}
 	
 	public boolean hasPlayer(int row, int col) {
@@ -68,9 +71,67 @@ public class StrategyGameController{
 		
 	//}
 	
-	//public List<int[]> getValidMoves(int row, int col, Team team){
+	public List<int[]> getValidMoves(int row, int col/*, Team team*/){
+		int speed = getMoveDistanceRemaining(row, col);
+		
+		List<int[]> possible = new ArrayList<int[]>();
+		
+		getValidMovesHelper(row, col, speed, possible);
+		
+		return possible;
+	}
 	
-	//}
+	private void getValidMovesHelper(int row, int col, int remaining, List<int[]> possible) {
+		if(remaining > 0) {
+		/* move up
+		 * if(can move up):
+		 * possible.add([row - 1, col])
+		 * getValidMovesHelper(row - 1, col, remaining - 1, possible)
+		*/
+		
+		/* move up right
+		 * if(can move up right):
+		 * possible.add([row - 1, col + 1])
+		 * getValidMovesHelper(row - 1, col + 1, remaining - 1, possible)
+		*/
+		
+		/* move right
+		 * if(can move right):
+		 * possible.add([row, col + 1])
+		 * getValidMovesHelper(row, col + 1, remaining - 1, possible)
+		*/
+		
+		/* move down right
+		 * if(can move down right):
+		 * possible.add([row + 1, col + 1])
+		 * getValidMovesHelper(row + 1, col + 1, remaining - 1, possible)
+		*/
+		
+		/* move down
+		 * if(can move down):
+		 * possible.add([row + 1, col])
+		 * getValidMovesHelper(row + 1, col, remaining - 1, possible) 
+		*/
+		
+		/* move down left
+		 * if(can move down left):
+		 * possible.add([row + 1, col - 1])
+		 * getValidMovesHelper(row + 1, col - 1, remaining - 1, possible) 
+		*/
+		
+		/* move left
+		 * if(can move left):
+		 * possible.add([row, col - 1])
+		 * getValidMovesHelper(row, col - 1, remaining - 1, possible)
+		 */
+		
+		/* move up left
+		 * if(can move up left):
+		 * possible.add([row - 1, col - 1])
+		 * getValidMovesHelper(row - 1, col - 1, remaining - 1, possible)
+		 */
+		}
+	}
 	
 	public boolean isOver() {
 		
