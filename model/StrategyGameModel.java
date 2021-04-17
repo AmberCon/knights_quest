1450d990fb991ef4977c44bf86a56f897206742c
Ember Chan <calvinjchan1@email.arizona.edu>
2021-04-16 20:18:18 -0700

    getTile Method

--- model/StrategyGameModel.java
+++ model/StrategyGameModel.java
@@ -6,7 +6,7 @@ package model;
  * 
  * The coordinates of the board for the game are as such:
  * 
- * 0 1 2 3 4 5 6 . . . x 
+ * 0 1 2 3 4 5 6 . . . col
  * 1
  * 2
  * 3
@@ -16,12 +16,25 @@ package model;
  * .
  * .
  * .
- * y
+ * row
  * 
  * 
  * @author Ember Chan
  *
  */
 public class StrategyGameModel{
+	
 	private Tile[][] board;
+	private Team currentTurn;
+	private String backgroundImageFileName;
+	
+	/**
+	 * Returns the tile at the given row and column
+	 * @param row the row of the tile to get, as an int
+	 * @param col the column of the tile to get, as an int
+	 * @return the Tile at the given row and column
+	 */
+	Tile getTile(int row, int col){
+		return board[row][col];
+	}
 }
