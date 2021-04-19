478dab6d816f301704ab80ba2d0314125bc6c469
dsitara <drakesitaraman@email.arizona.edu>
2021-04-19 11:14:49 -0700

    Pasted the Team enum in so I can refer to it

---
+++ model/Team.java
@@ -0,0 +1,11 @@
+package model;
+
+/**
+ * Enum representing the two different teams in the strategy game
+ * 
+ * @author Ember Chan
+ *
+ */
+public enum Team {
+	HUMAN, COMPUTER
+}
--- onboard/Piece.java
+++ onboard/Piece.java
@@ -3,5 +3,14 @@ package onboard;
 import java.io.Serializable;
 
 public abstract class Piece implements Serializable{
-	
+	int health;
+	int[] attackRange;
+	int attackDistance;
+	int[] defenseRange;
+	int moveDistanceRemaining;
+	boolean isDefended;
+	boolean hasAttackedOrDefended;
+	String team;
+	String spriteFileName;
+
 }
